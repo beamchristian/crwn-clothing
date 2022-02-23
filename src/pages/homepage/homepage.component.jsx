282fb911ccdca8930b735e10a183a3c06d87a4b2
beamchristian <ckbeamcoding@gmail.com>
2022-02-23 11:46:24 -0500

    collection preview component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Directory from '../components/Directory';
+import Directory from '../../components/Directory';
 
-import './Homepage.styles.scss';
+import './homepage.styles.scss';
 
 const HomePage = () => {
   return (
